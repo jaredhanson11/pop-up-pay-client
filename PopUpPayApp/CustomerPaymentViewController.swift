f424dc501042e74485b5197843a69a99f14b7cbb
Steven Chen <steven_c_chen@apple.com>
2017-07-13 13:21:29 -0700

    Add Apple Pay controller

--- PopUpPayApp/CustomerPaymentViewController.swift
+++ PopUpPayApp/CustomerPaymentViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PassKit
 
 class CustomerPaymentViewController: UIViewController {
 
@@ -15,11 +16,30 @@ class CustomerPaymentViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        button.center = self.view.center
+        self.view.addSubview(button)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.setTitle("Click to pay", for: .normal)
+        
+        button.addTarget(self, action: #selector(testButtonClicked), for: .touchUpInside)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func makePayment() {
+        
+    }
+    
+    func testButtonClicked(_ sender : Any) {
+        applePay(merchant_id: "whatever", description: NSObject())
+        
     }
     
 
@@ -32,5 +52,40 @@ class CustomerPaymentViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func applePay(merchant_id: String, description: NSObject) {
+        let request = PKPaymentRequest()
+        request.merchantIdentifier = merchant_id
+        request.supportedNetworks = [PKPaymentNetwork.visa, PKPaymentNetwork.masterCard]
+        request.merchantCapabilities = PKMerchantCapability.capability3DS
+        request.countryCode = "US"
+        request.currencyCode = "USD"
+        
+        // add summary items for transaction here
+        var summaryItems = [PKPaymentSummaryItem]()
+        
+        let item = PKPaymentSummaryItem(label: "to whoever", amount: NSDecimalNumber(value: 100))
+    
+        summaryItems.append(item)
+        request.paymentSummaryItems = summaryItems
 
+        // create the view controller for payment
+        let applePayController = PKPaymentAuthorizationViewController(paymentRequest: request)
+        applePayController.delegate = self
+
+        // show items
+        print("apple pay trigering")
+        self.present(applePayController, animated: true, completion: nil)
+    }
+}
+
+extension CustomerPaymentViewController: PKPaymentAuthorizationViewControllerDelegate {
+    @available(iOS 11.0, *)
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        completion(PKPaymentAuthorizationResult.init(status: PKPaymentAuthorizationStatus.success, errors: nil))
+    }
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
--- PopUpPayApp/QRScanViewController.swift
+++ PopUpPayApp/QRScanViewController.swift
@@ -1,9 +1,98 @@
 //
-//  QRScanViewController.swift
-//  PopUpPayApp
+//  ViewController.swift
+//  qr-scanner
 //
-//  Created by Steven Chen on 7/13/17.
-//  Copyright © 2017 Meghana Valluri. All rights reserved.
+//  Created by Andrew Grossfeld on 7/13/17.
+//  Copyright © 2017 Andrew Grossfeld. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+import AVKit
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        var input: AnyObject!
+        
+        do {
+            input = try AVCaptureDeviceInput(device: captureDevice!)
+        } catch {
+            print("error")
+            return
+        }
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(input as! AVCaptureInput)
+        
+        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        // Set delegate and use the default dispatch queue to execute the call back
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        // Move the message label to the top view
+        view.bringSubview(toFront: messageLabel)
+        
+        // Start video capture.
+        captureSession?.startRunning()
+        
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.green.cgColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubview(toFront: qrCodeFrameView!)
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QR code is detected"
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode{
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barCodeObject.bounds;
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
